cf2d6c593fb17726ef0bffef83b17d09e7cbf7b2
Alyona <alenka28816@gmail.com>
2024-09-26 16:00:13 +0200

    Add files via upload

---
+++ HW1/Main.java
@@ -0,0 +1,66 @@
+package Homeworks.HW1;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+
+       int num1 = 89;
+       Integer num12 = 89;
+       System.out.println(num1);
+            System.out.println(num12);
+        char num2 = 'G';
+        Character num21 = 'G';
+            System.out.println(num2);
+            System.out.println(num21);
+
+
+        byte num4 =  4;
+        Byte num41 = 4;
+            System.out.println(num4);
+            System.out.println(num41);
+        short num5 = 56;
+        Short num51 = 56;
+            System.out.println(num5);
+            System.out.println(num51);
+
+        float num6 = 4.7333436f;
+        Float num61 = 4.7333436f;
+            System.out.println(num6);
+            System.out.println(num61);
+        double num7 = 4.355453532;
+        Double num71 = 4.355453532;
+            System.out.println(num7);
+            System.out.println(num71);
+        long num8 =12121;
+        Long num81 = 12121l;
+            System.out.println(num8);
+            System.out.println(num81);
+
+
+       //Задание № 2--------------------------------------
+
+
+            int num9 = 345;
+            int res1 = num9/100 % 10;
+            int res2 = num9/10 % 10;
+            int res3 = num9 % 10;
+
+
+
+            System.out.printf("Число %d - > %d, %d, %d ", num9, res1, res2, res3);
+
+
+
+
+
+
+
+
+            //System.out.println(num1, num2, num3, num4, num5, num7, num8);
+        }
+
+
+
+
+    }
